89dfd9eaf6aacf886f34be0de29920618e46ef09
Josh Adell@ <josh.adell@gmail.com>
2023-07-06 12:12:26 -0400

    Isolate core processing logic to allow other forms of IO
    The processing logic is now separated from where input comes from and where output goes, allowing it to be reused for any number of IO method (TCP, socket, etc.) It is up to whatever wraps the processor to handle user input and display.

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const readline = require('node:readline/promises');
 const { stdin, stdout } = require('node:process');
-const { toLower, negate, isEmpty, toNumber } = require('lodash');
 const Stack = require('./lib/stack.js');
-const NoopCommand = require('./lib/commands/noop.js');
-const HelpCommand = require('./lib/commands/help.js');
-const QuitCommand = require('./lib/commands/quit.js');
-const ClearStackCommand = require('./lib/commands/clear-stack.js');
-const PrintStackCommand = require('./lib/commands/print-stack.js');
-const StackInPromptCommand = require('./lib/commands/stack-in-prompt.js');
-const CalcLineCommand = require('./lib/commands/calc-line.js');
+const Processor = require('./lib/processor.js');
 
 const rl = readline.createInterface({
     input: stdin,
@@ -24,47 +17,25 @@ process.on('SIGINT', () => {
     exit();
 });
 
-const FLOW_COMMANDS = [
-    NoopCommand,
-    HelpCommand,
-    QuitCommand,
-    ClearStackCommand,
-    PrintStackCommand,
-    StackInPromptCommand,
-];
+const options = {
+    stackInPrompt: false,
+};
+
+const stack = new Stack();
+const processor = new Processor();
 
 (async () => {
-    const options = {
-        stackInPrompt: false,
-    };
-
-    const stack = new Stack();
     let cmdExit = false;
     while (!cmdExit) {
         const prompt = (options.stackInPrompt ? JSON.stringify(stack.show()) : '') + '> ';
-        const line = toLower(await rl.question(prompt));
+        const line = await rl.question(prompt);
+        const result = processor.process(line, stack, options);
 
-        const commands = FLOW_COMMANDS.reduce((agg, curr) => {
-            return agg.concat(curr.fromLine(line))
-        }, []);
-
-        if (isEmpty(commands)) {
-            commands.push(...CalcLineCommand.fromLine(line));
-         }
-
-        let lastResult = null;
-        for (const command of commands) {
-            lastResult = command.execute({ stack, options });
-            if (lastResult.haltProcessing) {
-                break;
-            }
+        if (result.display) {
+            console.log(result.display);
         }
 
-        if (lastResult.display) {
-            console.log(lastResult.display);
-        }
-
-        if (lastResult.isExit) {
+        if (result.isExit) {
             cmdExit = true;
         }
     };
